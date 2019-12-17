0882e34cccfffbd47242e365688fe6127cc73e1f
DubmasterxD <dubmaster@tlen.pl>
2019-12-17 22:32:02 +0100

    Map made

--- PadTap/Assets/Scripts/MapMaker/Manager.cs
+++ PadTap/Assets/Scripts/MapMaker/Manager.cs
@@ -1,9 +1,32 @@
-﻿using UnityEngine;
+﻿using PadTap.Core;
+using System.Collections.Generic;
+using UnityEngine;
 
 namespace PadTap.MapMaker
 {
     public class Manager : MonoBehaviour
     {
-       
+        private AudioSource audioSource = null;
+        private Map map = null;
+        private Map.Point currentPoint = null;
+        private float selectionStartTime = 0;
+        private float selectionEndTime = 0;
+        private List<Map.Point> selection = null;
+        private float currentTime = 0;
+        private float playbackSpeed = 1;
+        private float deltaTime = 1;
+        private bool givenCopyright = false;
+
+        private int minRows = 1;
+        private int maxRows = 6;
+        private int minColumns = 1;
+        private int maxColumns = 6;
+        private float minIndicatorLifespan = 0.1f;
+        private float maxIndicatorLifespan = 10;
+
+        private void Update()
+        {
+            
+        }
     }
 }
